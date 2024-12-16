121ca6f020009642733adc86c75ec5ebd2c88d61
princekumar205086 <prince_kumar_mca21@jimsindia.org>
2024-12-16 16:44:17 +0530

    static file fixed

--- ptpi/urls.py
+++ ptpi/urls.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.authtoken import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('api-token-auth/', views.obtain_auth_token),
     path('api/', include('teacherhire.urls')),
     path('admin/', admin.site.urls),  # Ensure this line is present only once
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
