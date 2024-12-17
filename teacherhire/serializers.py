b91c19d522c4759d844cc1d326a07a19f4fa0e12
Sauravitachi <sauravkumar52778@gmail.com>
2024-12-17 22:47:15 +0530

    added recruiter  fixed

--- teacherhire/serializers.py
+++ teacherhire/serializers.py
@@ -27,7 +27,6 @@ class RecruiterRegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True)
     Fname = serializers.CharField(required=True)
     Lname = serializers.CharField(required=True)
-    is_recruiter = serializers.BooleanField(required=True)
 
     class Meta:
         model = CustomUser
