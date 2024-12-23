3013c5c0dc276e589cfb29f08af567b8be69e9da
princekumar205086 <prince_kumar_mca21@jimsindia.org>
2024-12-23 14:37:30 +0530

    added admin seeded data

---
+++ teacherhire/management/commands/seed_admin.py
@@ -0,0 +1,20 @@
+# teacherhire/management/commands/seed_admin.py
+
+from django.core.management.base import BaseCommand
+from teacherhire.models import CustomUser
+
+class Command(BaseCommand):
+    help = 'Seed the database with an admin user'
+
+    def handle(self, *args, **kwargs):
+        if not CustomUser.objects.filter(email='admin@example.com').exists():
+            CustomUser.objects.create_superuser(
+                email='admin@example.com',
+                username='admin',
+                password='adminpassword',
+                Fname='Admin',
+                Lname='User'
+            )
+            self.stdout.write(self.style.SUCCESS('Successfully created admin user'))
+        else:
+            self.stdout.write(self.style.WARNING('Admin user already exists'))
