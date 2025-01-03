d2bfece5215abe6cd96c9a81a9420eb623eeeb05
nidhi334 <knidhibharti8178@gmail.com>
2025-01-03 17:18:06 +0530

    report api done

--- teacherhire/views.py
+++ teacherhire/views.py
@@ -1840,5 +1840,23 @@ def insert_data(request):
         }
 
     return JsonResponse(response_data)
+class ReportViewSet(viewsets.ModelViewSet):    
+    # permission_classes = [IsAuthenticated]
+    # authentication_classes = [ExpiringTokenAuthentication] 
+    queryset= Report.objects.all()
+    serializer_class = ReportSerializer
+
+    
+    @action (detail=False,methods=['get'])
+    def count(self,request):
+        count = get_count(Report)
+        return Response({"Count":count})
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.delete()
+        return Response({"message": "Report deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
+
+
+
 
     
