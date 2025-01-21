bbf11002e24271fc2f0cdbbd32a94940b5929447
nidhi334 <knidhibharti8178@gmail.com>
2025-01-21 14:28:26 +0530

    preference  bug fixed

--- teacherhire/views.py
+++ teacherhire/views.py
@@ -961,7 +961,7 @@ class PreferenceViewSet(viewsets.ModelViewSet):
         try:
             return Preference.objects.get(user=self.request.user)
         except Preference.DoesNotExist:
-            raise NotFound({"detail": "Preference not found."})
+            raise NotFound({"detail": "Preference not found."}, status=status.HTTP_200_OK)
 
     def destroy(self, request, *args, **kwargs):
         instance = self.get_object()
