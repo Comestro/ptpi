3e323bd4ab247ac9427a40e1901838936d991940
KomalKumari17 <komalydv2760@gmail.com>
2025-01-22 13:52:16 +0530

    add ExamCenter model

--- teacherhire/models.py
+++ teacherhire/models.py
@@ -401,3 +401,14 @@ class Interview(models.Model):
     
     def __str__(self):
         return self.user.username
+    
+class ExamCenter(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    center_name = models.CharField(max_length=200, null=True, blank=True)
+    pincode = models.CharField(max_length=6, null=True, blank=True)
+    state = models.CharField(max_length=100, null=True, blank=True)
+    city = models.CharField(max_length=100, null=True, blank=True)
+    status = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.user.username
