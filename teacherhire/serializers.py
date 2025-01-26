decae7423571c635d629ab18bb8c9a3c849ed0bb
nidhi334 <knidhibharti8178@gmail.com>
2025-01-26 13:54:35 +0530

    remove untwanted line

--- teacherhire/serializers.py
+++ teacherhire/serializers.py
@@ -690,8 +690,4 @@ class ExamCenterSerializer(serializers.ModelSerializer):
         model = ExamCenter
         fields = "__all__"
 
-    def to_representation(self, instance):
-        representation = super().to_representation(instance)
-        representation['user'] = UserSerializer(instance.user).data
-        return representation
     
