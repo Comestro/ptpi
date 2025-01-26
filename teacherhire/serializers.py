ec899cf837fd23d58b41cafe8b97ccf70848afea
nidhi334 <knidhibharti8178@gmail.com>
2025-01-26 13:59:19 +0530

    representatio user in examcenter

--- teacherhire/serializers.py
+++ teacherhire/serializers.py
@@ -689,6 +689,11 @@ class ExamCenterSerializer(serializers.ModelSerializer):
     class Meta:
         model = ExamCenter
         fields = "__all__"
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation['user'] = UserSerializer(instance.user).data
+        return representation
         
 class TeacherReportSerializer(serializers.ModelSerializer):
     teacherskill = TeacherSkillSerializer(many=True, required=False)
