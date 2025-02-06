7a22a7a8d0cacdf36e5244f1c40761e640879738
PRINCE RAJ <rajkumar.prince2050@gmail.com>
2025-02-06 16:48:28 +0530

    seeder for all implemented in function added step in seeder to seed data in yml file

---
+++ teacherhire/management/commands/seed_data.py
@@ -0,0 +1,14 @@
+import requests
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    help = 'Makes a GET request to the exam center URL to insert data'
+
+    def handle(self, *args, **kwargs):
+        url = 'https://ptpi.tech/api/insert/data/'
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            self.stdout.write(self.style.SUCCESS('Data inserted successfully'))
+        else:
+            self.stdout.write(self.style.ERROR('Failed to insert data'))
