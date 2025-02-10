c7fba1093e47dc352eca94e30d3ee9eb783e6743
PRINCE RAJ <rajkumar.prince2050@gmail.com>
2025-02-10 16:24:07 +0530

    implemented command based seeder and added seeding data step in yml file

--- teacherhire/management/commands/seed_data.py
+++ teacherhire/management/commands/seed_data.py
@@ -5,7 +5,7 @@ class Command(BaseCommand):
     help = 'Makes a GET request to the exam center URL to insert data'
 
     def handle(self, *args, **kwargs):
-        url = 'https://ptpi.tech/api/insert/data/'
+        url = 'http://127.0.0.1:8000/api/admin/insert/data/'
         response = requests.get(url)
 
         if response.status_code == 200:
