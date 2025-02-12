7e7add92290895e6369d0b5060ff96c4a85db5bd
sadique-cws <sadique.hussain96@gmail.com>
2025-02-12 23:41:09 +0530

    add is_recruiter permission along with admin

--- teacherhire/permissions.py
+++ teacherhire/permissions.py
@@ -63,7 +63,7 @@ class IsAdminOrTeacher(BasePermission):
     # Allows access to admins and teachers.
 
     def has_permission(self, request, view):
-        return request.user.is_authenticated and (request.user.is_staff or request.user.is_teacher)
+        return request.user.is_authenticated and (request.user.is_staff or request.user.is_teacher or request.user.is_recruiter)
 
 
 class IsAuthenticatedReadOnly(BasePermission):
