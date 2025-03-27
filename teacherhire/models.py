9579be2a2663d2724d3c7f7fc802f2da14c7623d
KomalKumari17 <komalydv2760@gmail.com>
2025-03-27 17:34:44 +0530

    add attempt in teacherexamresult

--- teacherhire/models.py
+++ teacherhire/models.py
@@ -342,6 +342,7 @@ class TeacherExamResult(models.Model):
     isqualified = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     has_exam_attempt = models.BooleanField(default=False)
+    attempt = models.IntegerField(default=0)
     language = models.CharField(
         max_length=20,
         choices=[
