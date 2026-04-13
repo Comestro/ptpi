61ad288aa3f3cccd6ac5f7f608f3076568a6be7e
sadique-cws <sadique.hussain96@gmail.com>
2026-04-13 20:17:29 +0530

    chore: update static files configuration to use local directory instead of static root

--- ptpi/settings.py
+++ ptpi/settings.py
@@ -155,11 +155,11 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles' / 'static'
-# STATICFILES_DIRS = [
-#     BASE_DIR / "static",
-# ]
+# STATIC_URL = 'static/'
+# STATIC_ROOT = BASE_DIR / 'staticfiles' / 'static'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
