7e4308837c98a27ac12dda4a2ad9c5c6b6b61334
sadique-cws <sadique.hussain96@gmail.com>
2026-04-22 18:14:24 +0530

    chore: switch email backend to console for testing purposes

--- ptpi/settings.py
+++ ptpi/settings.py
@@ -198,11 +198,14 @@ PASSWORD_RESET_TIME_OUT = 900
 AUTH_USER_MODEL = 'teacherhire.CustomUser'
 
 # Email configuration
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.hostinger.com'
-EMAIL_PORT = 465  # Use 465 for SSL
-EMAIL_USE_TLS = False  # Set to False if using SSL
-EMAIL_USE_SSL = True  # Set to True if using SSL
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.hostinger.com'
+# EMAIL_PORT = 465  # Use 465 for SSL
+# EMAIL_USE_TLS = False  # Set to False if using SSL
+# EMAIL_USE_SSL = True  # Set to True if using SSL
+
+# Temporary Console Backend for testing
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
 DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_FROM')
